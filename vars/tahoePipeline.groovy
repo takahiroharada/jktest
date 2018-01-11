17e8aa1b0954e12d707f449bf7cab7d00eec3aae
takahiroharada <takahiroharada@gmail.com>
2018-01-11 10:46:18 -0800

    Test.

--- vars/tahoePipeline.groovy
+++ vars/tahoePipeline.groovy
@@ -44,7 +44,6 @@ def executeBuildUbuntu(String projectBranch)
             stash includes: 'scripts/**/*', name: 'scripts'
         }
     }
-    return retNode
 }
 
 def executeTestsGpu(String projectBranch)
