ba1036253249f3483bcd6eb05d7727a39c1cb6cb
takahiroharada <takahiroharada@gmail.com>
2018-01-10 21:48:14 -0800

    test.

---
+++ vars/tahoePipelineWin.groovy
@@ -0,0 +1,88 @@
+
+def checkOutBranchOrScm(String branchName, String repoName) {
+    if(branchName != "")
+    {
+        echo "checkout from user branch: ${branchName}; repo: ${repoName}"
+        checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
+            [$class: 'CleanCheckout'],
+            [$class: 'CheckoutOption', timeout: 30],
+            [$class: 'CloneOption', timeout: 30, noTags: false],
+            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
+            ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
+    }
+    else
+    {
+        echo 'checkout from scm options'
+	    checkout([
+	        $class: 'GitSCM',
+	        branches: scm.branches,
+	        doGenerateSubmoduleConfigurations: false,
+	        extensions: scm.extensions + [[$class: 'SubmoduleOption', parentCredentials: true]],
+	        userRemoteConfigs: scm.userRemoteConfigs
+	    ])
+    }
+}
+
+def executeBuildWin(String projectBranch)
+{
+    def retNode = {
+    	node("windows")
+    	{
+	    	stage("Check")
+	        {
+	            checkOutBranchOrScm(projectBranch, 'https://github.com/takahiroharada/firerender.git')
+	        }
+	        stage("Build") 
+	        {
+	            try 
+	            {
+	            	bat './scripts/build/win/buildTahoeMin.bat'
+	            }
+	            finally {
+	            }
+	        }
+	        stage("Test")
+	        {
+	        	sh './dist/release/bin/x86_64/UnitTest64 --gtest_list_tests'
+	        }
+	        stage("Artifact")
+	        {
+	        	archiveArtifacts artifacts: 'dist/release/**/*'
+	        }
+	    }
+    }
+    return retNode
+}
+
+def executeBuilds(String projectBranch)
+{
+    def tasks = [:]
+
+    tasks["Windows"] = executeBuildWin(projectBranch)
+
+    parallel tasks
+/*
+    def tasks = [:]    
+    testPlatforms.split(';').each()
+    {
+        tasks["${it}"] = executeTestWindows("${it}", projectBranch)
+    }
+    node("gpu${asicName}")
+    {
+	
+    }
+*/    
+}
+
+def call(String projectBranch='', String testPlatforms = 'AMD_RXVEGA;AMD_WX9100;AMD_WX7100', Boolean enableNotifications = true) {
+      
+    try 
+    {
+        timestamps {
+            executeBuilds(projectBranch)
+        }
+    }
+    finally 
+    {
+    }
+}
