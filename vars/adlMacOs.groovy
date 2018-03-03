6fc4b30b1eb7526ce5eea8aa7cab63d36f15816c
takahiroharada <takahiroharada@gmail.com>
2018-03-03 11:44:20 -0800

    Test.

---
+++ vars/adlMacOs.groovy
@@ -0,0 +1,73 @@
+def executeBuildsImpl(String os, String commandLinux, String commandWin,
+    def checkoutFunc, def postbuildFunc )
+{
+    def retNode = {
+	node("${os} && git")
+	{
+    	stage("Check-"+os)
+        {
+            checkoutFunc()
+        }
+        stage("Build-"+os) 
+        {
+            try 
+            {
+                if( isUnix() )
+                {
+                    sh commandLinux
+                }
+                else
+                {
+                	bat commandWin
+                }
+            }
+            finally {
+            }
+            postbuildFunc( os )
+        }
+    }
+    }
+    return retNode
+}
+
+
+def executeBuilds(String oses, String commandLinux,
+    def checkoutFunc, def postbuildFunc )
+{
+    def tasks = [:]
+
+    oses.split(',').each()
+    {
+        String os = "${it}"
+        tasks["Build-"+os] = executeBuildsImpl(os, commandLinux, commandWin, checkoutFunc, postbuildFunc )
+
+    }
+
+    parallel tasks
+}
+
+def checkoutImpl()
+{
+    checkOutBranchOrScm("", 'https://github.com/amdadvtech/adl.git')
+}
+
+def postBuildImpl( String os )
+{
+ 
+}
+
+def call(String testOses = "macos") 
+{
+    try 
+    {
+        String buildCmdMacOs = './scripts/macos/buildAndRunMetal.sh',
+        timestamps 
+        {
+            executeBuilds( testOses, buildCmdMacOs,
+                this.&checkoutImpl, this.&postBuildImpl )
+        }
+    }
+    finally 
+    {
+    }
+}
